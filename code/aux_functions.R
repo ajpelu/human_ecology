865ad63396bb8c5ad68c828f557fd1c6d05728c9
ajpelu <ajpelu@gmail.com>
2022-10-20 17:01:19 +0200

    include jaccard

--- code/aux_functions.R
+++ code/aux_functions.R
@@ -2,35 +2,37 @@
 
 
 #####################################################################
-#VIF FUNCTION. From Zuur et al. 2009 http://www.highstat.com/Books/Book2/HighstatLibV10.R
-#To use:  corvif(YourDataFile)
+# VIF FUNCTION. From Zuur et al. 2009 http://www.highstat.com/Books/Book2/HighstatLibV10.R
+# To use:  corvif(YourDataFile)
 corvif <- function(dataz) {
   dataz <- as.data.frame(dataz)
-  #correlation part
-  #cat("Correlations of the variables\n\n")
-  #tmp_cor <- cor(dataz,use="complete.obs")
-  #print(tmp_cor)
+  # correlation part
+  # cat("Correlations of the variables\n\n")
+  # tmp_cor <- cor(dataz,use="complete.obs")
+  # print(tmp_cor)
 
-  #vif part
-  form    <- formula(paste("fooy ~ ",paste(strsplit(names(dataz)," "),collapse=" + ")))
-  dataz   <- data.frame(fooy=1,dataz)
-  lm_mod  <- lm(form,dataz)
+  # vif part
+  form <- formula(paste("fooy ~ ", paste(strsplit(names(dataz), " "), collapse = " + ")))
+  dataz <- data.frame(fooy = 1, dataz)
+  lm_mod <- lm(form, dataz)
 
   cat("\n\nVariance inflation factors\n\n")
   print(myvif(lm_mod))
 }
-#Support function for corvif. Will not be called by the user
+# Support function for corvif. Will not be called by the user
 myvif <- function(mod) {
   v <- vcov(mod)
   assign <- attributes(model.matrix(mod))$assign
   if (names(coefficients(mod)[1]) == "(Intercept)") {
     v <- v[-1, -1]
     assign <- assign[-1]
-  } else warning("No intercept: vifs may not be sensible.")
+  } else {
+    warning("No intercept: vifs may not be sensible.")
+  }
   terms <- labels(terms(mod))
   n.terms <- length(terms)
   if (n.terms < 2) stop("The model contains fewer than 2 terms")
-  if (length(assign) > dim(v)[1] ) {
+  if (length(assign) > dim(v)[1]) {
     diag(tmp_cor) < -0
     if (any(tmp_cor == 1.0)) {
       return("Sample size is too small, 100% collinearity is present")
@@ -51,7 +53,7 @@ myvif <- function(mod) {
   if (all(result[, 2] == 1)) {
     result <- data.frame(GVIF = result[, 1])
   } else {
-    result[, 3] <- result[, 1]^(1/(2 * result[, 2]))
+    result[, 3] <- result[, 1]^(1 / (2 * result[, 2]))
   }
   invisible(result)
 }
@@ -60,14 +62,18 @@ myvif <- function(mod) {
 #####################################################################
 # functions to create posthocs tables
 posthoc.commons <-
-  function(modelo){emmeans(modelo, list(pairwise ~ commons), adjust = "tukey")}
-posthoc.transh <- function(modelo){
-  emmeans(modelo, list(pairwise ~ transh), adjust = "tukey")}
-posthoc.int <- function(modelo){
-  emmeans(modelo, list(pairwise ~ commons | transh), adjust = "tukey")}
+  function(modelo) {
+    emmeans(modelo, list(pairwise ~ commons), adjust = "tukey")
+  }
+posthoc.transh <- function(modelo) {
+  emmeans(modelo, list(pairwise ~ transh), adjust = "tukey")
+}
+posthoc.int <- function(modelo) {
+  emmeans(modelo, list(pairwise ~ commons | transh), adjust = "tukey")
+}
 
 
-tabla_postHoc_commons <- function(modelo){
+tabla_postHoc_commons <- function(modelo) {
   posthoc <- emmeans(modelo, list(pairwise ~ commons), adjust = "tukey")
   as.data.frame(posthoc$`pairwise differences of commons`) %>%
     rename(commons = 1) %>%
@@ -75,7 +81,7 @@ tabla_postHoc_commons <- function(modelo){
     kable_paper("hover", full_width = F)
 }
 
-tabla_postHoc_transh <- function(modelo){
+tabla_postHoc_transh <- function(modelo) {
   posthoc <- emmeans(modelo, list(pairwise ~ transh), adjust = "tukey")
   as.data.frame(posthoc$`pairwise differences of transh`) %>%
     rename(transh = 1) %>%
@@ -83,7 +89,7 @@ tabla_postHoc_transh <- function(modelo){
     kable_paper("hover", full_width = F)
 }
 
-tabla_postHoc_int <- function(modelo){
+tabla_postHoc_int <- function(modelo) {
   posthoc <- emmeans(modelo, list(pairwise ~ commons | transh), adjust = "tukey")
   as.data.frame(posthoc$`pairwise differences of commons | transh`) %>%
     rename(commons = 1) %>%
@@ -91,7 +97,7 @@ tabla_postHoc_int <- function(modelo){
     kable_paper("hover", full_width = F)
 }
 
-tabla_postHoc_transhcommons <- function(modelo){
+tabla_postHoc_transhcommons <- function(modelo) {
   posthoc <- emmeans(modelo, list(pairwise ~ transh | commons), adjust = "tukey")
   as.data.frame(posthoc$`pairwise differences of transh | commons`) %>%
     rename(transh = 1) %>%
@@ -101,7 +107,7 @@ tabla_postHoc_transhcommons <- function(modelo){
 
 #####################################################################
 # functions to formatAnova
-formatAnova <- function(df, yvar){
+formatAnova <- function(df, yvar) {
   df$variable <- yvar
   df$factor <- rownames(df)
   df %>%
@@ -113,7 +119,7 @@ formatAnova <- function(df, yvar){
 #####################################################################
 # functions to plot individually
 colores <- c("dodgerblue4", "goldenrod1")
-plot_inter <- function(df, response){
+plot_inter <- function(df, response) {
   df |>
     filter(nameF == response) |>
     ggplot(aes(y = mean, x = commonsF, group = transh, colour = transh)) +
@@ -129,7 +135,6 @@ plot_inter <- function(df, response){
     labs(x = "", y = parse(text = response)) +
     scale_colour_manual("Transhumance", values = colores) +
     guides(color = guide_legend(direction = "horizontal"))
-
 }
 
 
@@ -138,11 +143,11 @@ plot_inter <- function(df, response){
 ##### Tabla coeficientes
 tabla_coef <- function(modelo) {
   broom::tidy(modelo, effects = "fixed") |>
-    mutate_at(3:5, ~round(., 2)) |>
-    mutate_at(7, ~round(., 3)) |>
+    mutate_at(3:5, ~ round(., 2)) |>
+    mutate_at(7, ~ round(., 3)) |>
     kbl() |>
     kable_paper("hover", full_width = F)
-  }
+}
 
 
 
@@ -167,6 +172,3 @@ tabla_coef <- function(modelo) {
 # summary(model.avg(model_set, subset = delta <= 2))
 #
 # ss <- subset(model_set, delta <= 2, recalc.weights=FALSE)
-
-
-
